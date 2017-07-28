abe8542027cff5022fa68e36b1f2078cbc6912e6
Brennan Brown <brennan.brown@edu.sait.ca>
2017-07-28 21:10:30 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,8 @@
+# A simple example of an while loop in Ruby.
 def using_while
 	levitation_force = 6
-	
-	#your code here
+	while levitation_force < 10
+        puts "Wingardium Leviosa"
+        levitation_force += 1
+    end
 end
-
-
