51282c8be7b58d70e030dbb743fb81c7e0ba5759
Juan Diego Gomez Peña <juandi.gomezp@utadeo.edu.co>
2019-08-06 14:51:46 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,10 @@
 def using_until
   levitation_force = 6
   #your code here
-    
+    until levitation_force == 10
+      puts "Almost there"
+      levitation_force += 1
+    end
+      puts "Wingardium Leviosa"
+    end
 end
-
